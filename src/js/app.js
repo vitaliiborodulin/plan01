72526ca23b3234076c4b8b84a4938786baa2c9a4
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-04-11 08:25:38 +0300

    update 11042020

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,7 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
-//= ../../node_modules/inputmask/dist/jquery.inputmask.min.js
-//= ../../node_modules/imagelightbox/dist/imagelightbox.min.js
 
 $(function(){
-
-    //= components/inputmask.js
-    //= components/header-menu.js
-    //= components/imagelightbox.js
-
+	
     console.log(1);
 
 });
